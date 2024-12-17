cfb9ada8ccfba47ec8b1b4e1d7483e9fd64dc93e
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-17 15:01:28 -0300

    Update Candidato.java

--- jobify-back/src/main/java/lp/jobify/model/Candidato.java
+++ jobify-back/src/main/java/lp/jobify/model/Candidato.java
@@ -5,19 +5,28 @@ import lombok.Setter;
 
 @Setter
 @Getter
+import javax.persistence.*;
+
+@Entity
 public class Candidato {
-    // Getters e Setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String nome;
     private String email;
     private String habilidades;
     private String cargo;
 
-    // Construtor
+    // Construtor (sem argumentos é necessário para o JPA)
+    public Candidato() {}
+
+    // Construtor com argumentos
     public Candidato(String nome, String email, String habilidades, String cargo) {
         this.nome = nome;
         this.email = email;
         this.habilidades = habilidades;
-        this.cargo= cargo;
+        this.cargo = cargo;
     }
 
 
