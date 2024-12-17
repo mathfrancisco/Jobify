a944ea4b9ece717efbefc47ffd4ae1a5f530f174
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-17 10:39:54 -0300

    Update Candidato.java

--- jobify-back/src/main/java/lp/jobify/model/Candidato.java
+++ jobify-back/src/main/java/lp/jobify/model/Candidato.java
@@ -10,12 +10,14 @@ public class Candidato {
     private String nome;
     private String email;
     private String habilidades;
+    private String cargo;
 
     // Construtor
-    public Candidato(String nome, String email, String habilidades) {
+    public Candidato(String nome, String email, String habilidades, String cargo) {
         this.nome = nome;
         this.email = email;
         this.habilidades = habilidades;
+        this.cargo= cargo;
     }
 
 
