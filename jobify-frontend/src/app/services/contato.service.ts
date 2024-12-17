5939689b86e761f0dd21e859a91986a521d28d5e
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-16 23:12:28 -0300

    Atualizar o contato.service.ts

--- jobify-frontend/src/app/services/contato.service.ts
+++ jobify-frontend/src/app/services/contato.service.ts
@@ -1,9 +1,18 @@
+// contato.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Mensagem } from '../models/mensagem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContatoService {
+  private apiUrl = 'http://localhost:8080/api'; // URL da sua API Spring
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  enviarMensagem(mensagem: Mensagem): Observable<any> {
+    return this.http.post(`${this.apiUrl}/contato`, mensagem);
+  }
 }
