3fdf83ee8215d6c3da7f9f666f2a59a7f5c15db9
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-17 15:22:29 -0300

    Update RecrutadorRepository.java

--- jobify-back/src/main/java/lp/jobify/model/RecrutadorRepository.java
+++ jobify-back/src/main/java/lp/jobify/model/RecrutadorRepository.java
@@ -1,3 +1,4 @@
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import lp.jobify.model.Candidato;
 
 public interface RecrutadorRepository extends JpaRepository<Recrutador, Long> { }
