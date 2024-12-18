aafcc2a35b6647718d5198c40cad90f13c27c65b
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-18 15:33:18 -0300

    Update VagaRepository.java

--- jobify-back/src/main/java/lp/jobify/repository/VagaRepository.java
+++ jobify-back/src/main/java/lp/jobify/repository/VagaRepository.java
@@ -1,9 +1,11 @@
-package lp.jobify.repository;// VagaRepository.java
+
 import lp.jobify.model.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
     List<Vaga> findByRecrutadorId(Long recrutadorId);
 }
