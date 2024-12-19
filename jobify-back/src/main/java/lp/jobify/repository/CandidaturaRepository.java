87cc485ea0ecf6e9f928849e0b2c4375d622a0f7
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-19 14:29:13 -0300

    Create CandidaturaRepository.java

---
+++ jobify-back/src/main/java/lp/jobify/repository/CandidaturaRepository.java
@@ -0,0 +1,12 @@
+// CandidaturaRepository.java
+import lp.jobify.model.Candidatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CandidaturaRepository extends JpaRepository<Candidatura, Long> {
+    List<Candidatura> findByVagaId(Long vagaId);
+    List<Candidatura> findByCandidatoId(Long candidatoId);
+}
