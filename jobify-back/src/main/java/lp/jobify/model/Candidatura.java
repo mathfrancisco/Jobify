c8280aa61736f55b6784a461db1a16753d4beabf
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-12-19 14:29:33 -0300

    Create Candidatura.java

---
+++ jobify-back/src/main/java/lp/jobify/model/Candidatura.java
@@ -0,0 +1,35 @@
+
+import lp.jobify.model.Candidato;
+import lp.jobify.model.Vaga;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Candidatura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "vaga_id")
+    private Vaga vaga;
+
+    @ManyToOne
+    @JoinColumn(name = "candidato_id")
+    private Candidato candidato;
+
+    private String status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAplicacao;
+
+}
