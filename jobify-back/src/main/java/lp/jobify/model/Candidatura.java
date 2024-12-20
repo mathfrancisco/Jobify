b2b4f82a979acac2149c4f65b55a9b110649a72c
mathfrancisco2 <math.francisco2@gmail.com>
2024-12-19 22:32:04 -0300

    att

--- jobify-back/src/main/java/lp/jobify/model/Candidatura.java
+++ jobify-back/src/main/java/lp/jobify/model/Candidatura.java
@@ -1,17 +1,10 @@
 package lp.jobify.model;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
-
-@ToString
-@RequiredArgsConstructor
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class Candidatura {
 
@@ -29,11 +22,71 @@ public class Candidatura {
 
     private String status;
 
-    @Temporal(TemporalType.TIMESTAMP)
     private LocalDate dataAplicacao;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Vaga getVaga() {
+        return vaga;
+    }
+
+    public void setVaga(Vaga vaga) {
+        this.vaga = vaga;
+    }
+
+    public Candidato getCandidato() {
+        return candidato;
+    }
+
+    public void setCandidato(Candidato candidato) {
+        this.candidato = candidato;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDataAplicacao() {
+        return dataAplicacao;
+    }
 
     public void setDataAplicacao(LocalDate dataAplicacao) {
         this.dataAplicacao = dataAplicacao;
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Candidatura that = (Candidatura) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Candidatura{" +
+                "id=" + id +
+                ", vaga=" + vaga +
+                ", candidato=" + candidato +
+                ", status='" + status + '\'' +
+                ", dataAplicacao=" + dataAplicacao +
+                '}';
+    }
 }
